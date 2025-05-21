c962472bf2e3440ba3bf2849609f4e378a0c10e3
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-21 18:53:58 +0100

    Add product model, views, and template for displaying available ritual boxes

--- boxes/views.py
+++ boxes/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    products = Product.objects.filter(is_available=True)
+    boxes = products.filter(category__icontains="box")
+    items = products.exclude(category__icontains="box")
+
+    return render(
+        request,
+        "boxes/all_products.html",
+        {
+            "boxes": boxes,
+            "items": items,
+        },
+    )
