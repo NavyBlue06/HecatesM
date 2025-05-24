aad5fde8889e2b10048ba49ee7320d1e8273e7ae
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-24 19:42:24 +0100

    Add cart functionality; implement add_to_cart_item view and update URLs

--- hecatemarket/urls.py
+++ hecatemarket/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.market_landing, name="market_landing"),
-    path("items/", views.market_items, name="market_items"),  # magical items
+    path("items/", views.market_items, name="market_items"),
+    path("add_to_cart/<int:item_id>/", views.add_to_cart_item, name="add_to_cart_item"),
 ]
--- hecatemarket/views.py
+++ hecatemarket/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from hecatemarket.models import MagicalItem # correct name of model Navahlicious!
+from cart.cart import Cart
+from django.contrib import messages
+
 
 def market_landing(request):
     return render(request, 'hecatemarket/market_landing.html')
 
+
 def market_items(request):
     items = MagicalItem.objects.filter(is_available=True)
     return render(request, 'hecatemarket/market_items.html', {'items': items})
+
+
+def add_to_cart_item(request, item_id):
+    item = get_object_or_404(MagicalItem, pk=item_id)
+    cart = Cart(request)
+    cart.add(product=item)
+    messages.success(request, f"{item.name} was added to your cart.")
+    return redirect("market_items")
