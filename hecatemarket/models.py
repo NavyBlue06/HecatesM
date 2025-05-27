00a74c804f665c8baaf922b53891bc82df95ca6c
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-27 20:12:21 +0100

    Switch MagicalItem to ImageField with Cloudinary storage

--- hecatemarket/models.py
+++ hecatemarket/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from cloudinary.models import CloudinaryField  #  cloudinary
+
 
 class MagicalItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = CloudinaryField("image", blank=True, null=True)  #  cloudinary
+    image = models.ImageField(
+        upload_to="market_items/", blank=True, null=True
+    )  # back to ImageField
     is_available = models.BooleanField(default=True)
 
     def __str__(self):
